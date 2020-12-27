4b0d066e538a2491780f1b6c96638dc3c39c89e2
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-27 04:51:31 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -64,44 +64,30 @@
                 <span class="ion-android-close"></span>
             </button>
 
-            <h4 class="mb-0"><span class="text-main-1">Sign</span> In</h4>
+            <h4 class="mb-0"><span class="text-main-1">Přihlásit</span> se</h4>
 
             <div class="nk-gap-1"></div>
-            <form action="#" class="nk-form text-white">
+            <form action="includes/login.inc.php" class="nk-form text-white">
                 <div class="row vertical-gap">
                     <div class="col-md-6">
-                        Use email and password:
+                        Použij email a heslo:
 
                         <div class="nk-gap"></div>
-                        <input type="email" value="" name="email" class=" form-control" placeholder="Email">
-
+                        <input type="text" name="mailuid" class=" form-control" placeholder="Email">
                         <div class="nk-gap"></div>
-                        <input type="password" value="" name="password" class="required form-control" placeholder="Password">
-                    </div>
-                    <div class="col-md-6">
-                        Or social account:
-
-                        <div class="nk-gap"></div>
-
-                        <ul class="nk-social-links-2">
-                            <li><a class="nk-social-facebook" href="#"><span class="fab fa-facebook"></span></a></li>
-                            <li><a class="nk-social-google-plus" href="#"><span class="fab fa-google-plus"></span></a></li>
-                            <li><a class="nk-social-twitter" href="#"><span class="fab fa-twitter"></span></a></li>
-                        </ul>
+                        <input type="password" name="pwd" class="required form-control" placeholder="Heslo">
                     </div>
                 </div>
 
                 <div class="nk-gap-1"></div>
                 <div class="row vertical-gap">
-                    <div class="col-md-6">
-                        <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-block">Sign In</a>
-                    </div>
+                    <button type="submit" name="login-submit">Login</button>
                     <div class="col-md-6">
                         <div class="mnt-5">
-                            <small><a href="#">Forgot your password?</a></small>
+                            <small><a href="#">Zapomněl jsi heslo?</a></small>
                         </div>
                         <div class="mnt-5">
-                            <small><a href="#">Not a member? Sign up</a></small>
+                            <small><a href="#">Nejsi členem. Registruj se!</a></small>
                         </div>
                     </div>
                 </div>
