716fcb2a04f8a9ae96dfaa3eb7e3e3d67136504c
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2021-01-11 17:44:27 +0100

    Add files via upload

---
+++ includes/reset-request.inc.php
@@ -0,0 +1,62 @@
+<?php
+
+if (isset($_POST["reset-request-submit"]) {
+
+  $selector = bin2hex(random_bytes(8));
+  $token = random_bytes(32);
+
+  $url = "http://188.175.145.172/forgottenpwd/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
+
+  $expires = date("U") + 1800;
+
+  require 'dbh.inc.php;';
+
+  $userEmail = $_POST["email"];
+
+  $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    echo "There was an error!";
+    exit();
+  }
+  else {
+    mysqli_stmt_bind_param($stmt, "s", $userEmail);
+    mysqli_stmt_execute($stmt);
+  }
+
+  $sql = "INSERT INTO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?);";
+
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    echo "There was an error!";
+    exit();
+  }
+  else {
+    $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "ssss", $userEmail, $selector, $hashedToken, $expires);
+    mysqli_stmt_execute($stmt);
+  }
+
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
+
+  $to = $userEmail;
+
+  $subject = 'Reset hesla na 188.175.145.172';
+
+  $message = '<p>Dostali pozadavek o zmenu hesla. Prosim o pokracovani na tenhle link. Muzete tento
+  email ignorovat</p>';
+  $message .= '<p>Zde je vas link na zmeneni hesla: </br>';
+  $message .= '<a href="' . $url . '">' . $url .'</a></p>';
+
+  //$headers = "From, reply to";
+  $headers = "Content-type: text/html\r\n";
+
+  mail($to, $subject, $message, $headers);
+
+  header("Location: ../reset-password.php?reset=success");
+
+}
+else {
+  header("Location: ../index.php");
+}
